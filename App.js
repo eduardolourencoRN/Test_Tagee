702d089c018b8905ea1896b20f4468694a83b68c
sr-scanor <eduardo_araujo2030@hotmail.com>
2024-02-28 21:59:09 -0300

    adicionado splashScreen

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
 import LoginScreen from './src/screens/Login';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Impede que a splash screen seja ocultada automaticamente
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    useEffect(() => {
+        // Oculta a splash screen após 3 segundos
+        setTimeout(async () => {
+            await SplashScreen.hideAsync();
+        }, 3000); // 3000 milissegundos = 3 segundos
+    }, []);
+
     return <LoginScreen />;
 }
